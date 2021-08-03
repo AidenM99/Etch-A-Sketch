e3e9792c2278934a380b1f43d049916303798ae5
Aiden McNaughton <aiden-99@hotmail.co.uk>
2021-08-03 13:37:19 +0100

    Add for loop to create row of 16 divs

---
+++ scripts.js
@@ -0,0 +1,10 @@
+for(i=0; i<16; i++){
+const container = document.querySelector(".grid-container");
+const div = document.createElement("div");
+
+div.style.height = "50px";
+div.style.width = "50px";
+div.style.border = "solid black";
+
+container.appendChild(div);
+}
