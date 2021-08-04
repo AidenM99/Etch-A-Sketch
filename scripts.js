578544c630039ac33350af0d590b2439a83ec406
Aiden McNaughton <aiden-99@hotmail.co.uk>
2021-08-04 14:48:14 +0100

    Make dynamic 16x16 grid and add colour change to grid squares on mouseover event

--- scripts.js
+++ scripts.js
@@ -1,10 +1,38 @@
-for(i=0; i<16; i++){
-const container = document.querySelector(".grid-container");
-const div = document.createElement("div");
+/*****Create grid *****/
+for (i = 0; i < 16; i++) {
+    const grid = document.querySelector(".grid");
+    const row = document.createElement("div");
+    grid.appendChild(row);
+    for (j = 0; j < 16; j++) {
+        const column = document.createElement("div");
 
-div.style.height = "50px";
-div.style.width = "50px";
-div.style.border = "solid black";
+        column.style.height = "15px";
+        column.style.width = "15px";
+        column.style.border = "solid black 1px";
+        column.classList.add("row");
 
-container.appendChild(div);
-}
+        grid.appendChild(column);
+    };
+};
+
+/*****Random color picker for grid square*****/
+const gridSquare = document.querySelectorAll(".row");
+
+gridSquare.forEach(function (item) {
+    item.addEventListener("mouseover", function () {
+        const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+        let hexColour = "#";
+        for (i = 0; i < 6; i++) {
+            hexColour += hex[Math.floor(Math.random() * hex.length)];
+        };
+        item.style.backgroundColor = hexColour;
+    });
+});
+
+
+
+
+
+
+
+
